b6e6b7af190b35d090cc146cf0797f20842e1fd8
BHowland-FoodEnthusiast <brandon.howland@vtsd.com>
2025-01-10 13:11:30 -0500

    tkinter frames, labels, text and buttons! exciting!

--- api stuffs.py
+++ api stuffs.py
@@ -4,21 +4,42 @@ import tkinter as tk
 
 # main window
 root = tk.Tk()
-root.wm_geometry("200x200")
+root.wm_geometry("250x200")
 
 #create empty frame (new window for components in root)
 frame_login = tk.Frame(root)
-
+frame_auth = tk.Frame(root)
 #activate grid in said new frame
-frame_login.grid()
+frame_login.grid(row=0, column=0, sticky="news")
+frame_auth.grid(row=0, column=0, sticky="news")
+
+def login():
+    frame_auth.tkraise()
 
 #frame login widgets
-lbl_username = tk.Label(frame_login, text='Username:', fg='blue', font=('Times 200'))
-lbl_username.pack(padx=50, pady=50)
+lbl_username = tk.Label(frame_login, text='Username:', fg='blue', font='Times 15')
+lbl_username.pack(padx=50)
+
 
 #text box for username
 ent_username = tk.Entry(frame_login, bd=3)
-ent_username.pack(pady=5)
+ent_username.pack()
 
 
+#both above but for the password
+lbl_password = tk.Label(frame_login, text='Password:', fg='blue', font='Times 15')
+lbl_password.pack(padx=50)
+
+ent_password = tk.Entry(frame_login, bd=3)
+ent_password.pack()
+
+#button for logging in
+button_login = tk.Button(frame_login, text='Login', command=login)
+button_login.pack(pady=10)
+
+#auth label
+lbl_authorized = tk.Label(frame_auth, text='Authorization screen', fg='blue', font='Times 15')
+lbl_authorized.pack(padx=50)
+
+frame_login.tkraise()
 root.mainloop()
